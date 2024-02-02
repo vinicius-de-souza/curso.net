59dab70dc46893a44f65c511fefc2a3ec4c3894a
Vinicius de Souza <viniciusdesouzafernando@gmail.com>
2024-02-02 15:41:53 -0300

    Exercicio triangulos OO

---
+++ TriangulosEx1OO/TriangulosEx1OO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangulosEx1OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Digite os lados do triangulo X");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite os lados do triangulo Y");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Area do triangulo X = {x.CalculaArea():0.0000}");
+            Console.WriteLine($"Area do triangulo Y = {y.CalculaArea():0.0000}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TriangulosEx1OO/TriangulosEx1OO/Triangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangulosEx1OO
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double CalculaArea()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
