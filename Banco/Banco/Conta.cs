4518fc083d2bef87d8173a59c8931814c2d30719
Vinicius de Souza <viniciusdesouzafernando@gmail.com>
2024-02-22 23:16:21 -0300

    banco pratica poo

---
+++ Banco/Banco/Conta.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class Conta
+    {
+        // atributos privados
+        private double Saldo;
+        private short NumeroConta;
+        // propriedades autoimplementadas
+        public string _nome { get; private set; }
+        // construtores
+        public Conta (string nome, short numeroConta)
+        {
+            _nome = nome;
+            NumeroConta = numeroConta;
+        }
+
+        public Conta(string nome, short numeroConta, double saldo)
+        {
+            _nome = nome;
+            NumeroConta = numeroConta;
+            Saldo = saldo;
+        }
+        // propriedades customizadas
+
+        // metodos gerais
+
+        public void AdicionarSaldo(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(double valor)
+        {
+            Saldo -= valor;
+        }
+
+        public override string ToString()
+        {
+            return $"\nDados da conta:\nConta: {NumeroConta}, Titular: {_nome}, Saldo: ${Saldo.ToString("F2", CultureInfo.InvariantCulture)}\n";
+        }
+    }
+}
---
+++ Banco/Banco/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+            Console.WriteLine("----BANCO----");
+            Console.WriteLine("Entre o numero da conta: ");
+            short NumeroConta = short.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o nome do titular: ");
+            string Nome = Console.ReadLine();
+            Console.WriteLine("Haverá um depósito inicial? (s/n): ");
+            char Opcao = char.Parse(Console.ReadLine());
+            Conta conta = new Conta(Nome, NumeroConta);
+
+            if (Opcao == 's')
+            {
+                Console.WriteLine("Entre o valor de deposito inicial: ");
+                valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta.AdicionarSaldo(valor);
+            }
+
+            Console.WriteLine(conta.ToString());
+
+
+            Console.WriteLine("Entre um valor para depósito: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.AdicionarSaldo(valor);
+            Console.WriteLine(conta.ToString());
+
+            Console.WriteLine("Entre um valor para saque: ");
+            valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Sacar(valor-5);
+            Console.WriteLine(conta.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
